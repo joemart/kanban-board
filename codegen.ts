156c014d76d8331c82f9b4b6541bea4685d7aef5
Joseph Martire <joemart06@gmail.com>
2026-01-23 07:27:15 -0400

    docs: Remove unnecessary documentation

--- codegen.ts
+++ codegen.ts
@@ -1,29 +1,3 @@
-// import type { CodegenConfig } from '@graphql-codegen/cli';
-
-// const config: CodegenConfig = {
-//   overwrite: true,
-//   schema: "./src/graphql/schema.graphql",
-//   documents: ['./src/graphql/**/*.graphql', 
-//     './src/**/*.{tsx,ts,js,jsx}',
-//     '!./src/**/*.test.{tsx,ts,js,jsx}',
-//     '!./src/**/*.spec.{tsx,ts,js,jsx}',
-//     '!./node_modules/**/*'],
-//   generates: {
-//     "./src/graphql/__generated__/": {
-//       preset: "client",
-//       plugins: []
-//       // plugins: ["typescript", "typescript-operations", "typescript-react-apollo"]
-//     },
-//     "./graphql.schema.json": {
-//       plugins: ["introspection"]
-//     }
-//   }
-// };
-
-// export default config;
-
-
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
 import dotenv from "dotenv"
 
@@ -35,18 +9,13 @@ const config: CodegenConfig = {
   schema: `${process.env.NHOST_GRAPHQL_URL}`,
   documents: ['./src/graphql/**/*.graphql'],
   config: {
-    ignoreNoFields: true,  // This might help
+    ignoreNoFields: true,
   },
   generates: {
     "./src/graphql/__generated__/": {
        preset: "client",
-      
-      
-    
     },
-    // "./graphql.schema.json": {
-    //   plugins: ["introspection"]
-    // }
+
   }
 };
 
