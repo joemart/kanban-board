108ac7cae117cce50dec7bf0bff4a863609f1506
Joseph Martire <joemart06@gmail.com>
2026-02-13 12:22:52 -0400

    feat: Add ApolloLink class

--- src/app/apollo-client.ts
+++ src/app/apollo-client.ts
@@ -1,34 +1,42 @@
 'use client'
-import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink, Observable, split } from '@apollo/client'
 import {GraphQLWsLink} from "@apollo/client/link/subscriptions"
 import { OperationTypeNode, Kind } from 'graphql';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { createClient } from "graphql-ws";
 import { nhost } from '../lib/nhost';
-import { setContext } from '@apollo/client/link/context' 
 
- 
-
-  const authLink = setContext(async (_, { headers }) => {
-  
-    const session = nhost.auth.getSession()
-    const token = nhost.auth.getAccessToken()
-    const userId = nhost.auth.getUser()
-
-    return {
-      headers: {
-        ...headers,
-        'Authorization': token ? `Bearer ${token}` : '',
-        'x-hasura-user-id': userId || '',               
-        'x-hasura-role': 'user',
+const authLink = new ApolloLink((operation, forward) => {
+  return new Observable(observer => {
+    const setHeaders = async () => {
+      try {
+        const token = await nhost.auth.getAccessToken();
+        const user = nhost.auth.getUser();
+        
+        operation.setContext(({ headers = {} }) => ({
+          headers: {
+            ...headers,
+            Authorization: token ? `Bearer ${token}` : '',
+            'x-hasura-user-id': user?.id || '',
+            'x-hasura-role': 'user',
+          }
+        }));
+        
+        forward(operation).subscribe(observer);
+      } catch (error) {
+        observer.error(error);
       }
-    }
-})
+    };
+    
+    setHeaders();
+  });
+});
 
  const httpLink = createHttpLink({
     uri: nhost.graphql.httpUrl,
 
   });
+
 
   const wsLink = new GraphQLWsLink(
     createClient({
@@ -44,21 +52,12 @@ import { setContext } from '@apollo/client/link/context'
         connected: ()=>console.log("Connected websocket"),
         error: ()=> console.log("Error connecting websocket"),
         closed: (event)=> console.log("Socket closed" , event)
-      },
-      // connectionParams: ()=>{
-      //   return {
-      //     headers: {
-      //       'x-hasura-user-id': userId || '', 
-      //     }
-      //   }
-      // }
-      // keepAlive: 10000,
-      // lazy:false
+      }
       
     })
   )
 
-  const splitLink = ApolloLink.split(
+  const splitLink = split(
     ({query})=>{
       const definition = getMainDefinition(query)
       return definition.kind === Kind.OPERATION_DEFINITION && 
@@ -67,7 +66,6 @@ import { setContext } from '@apollo/client/link/context'
   authLink.concat(httpLink))
 
 export function makeClient() {
-
   return new ApolloClient({
     link: splitLink,
     cache: new InMemoryCache(),
