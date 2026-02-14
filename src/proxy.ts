2867f7174918bc6b10e9e77181562923f7f09e5a
Joseph Martire <joemart06@gmail.com>
2026-02-14 09:58:42 -0400

    feat(middleware): Add middleware that handles cookie

---
+++ src/proxy.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export default function proxy(request: NextRequest) {
+  // Your middleware logic here
+
+  const token = request.cookies.get("nhost-token")
+
+  // console.log('Middleware running on:', request.url)
+    if (request.nextUrl.pathname === ('/') && token) {
+      return NextResponse.redirect(new URL("/signin", request.url))
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    // Exclude API routes, static files, image optimizations, and .png files
+    '/((?!api|_next/static|_next/image|.*\\.png$).*)',
+  ],
+}
