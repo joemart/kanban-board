691d0cfce8869388aaf518e646b49470494f89ca
Joseph Martire <joemart06@gmail.com>
2026-02-14 12:03:45 -0400

    feat: Add navbar custom buttons

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,19 +1,21 @@
 "use client"
 import { useAuthenticated , useSignOut } from "@nhost/nextjs";
-import Link from "next/link";
+import { Menubar } from "@/ui/menubar";
 
+import Link from "next/link";
+import KanbanButton from "../KanbanButton";
 
 const Navbar = () => {
     const isAuthenticated = useAuthenticated()
     const {signOut} = useSignOut()
 
-
-
-    return ( <section>
-       <Link href={"/"}>Home</Link>
-        {isAuthenticated ? <button className=" cursor-pointer" onClick={signOut}>Sign out</button> : <div><Link href={"/signin"}>Sign In</Link></div>}
-        <Link href={"/register"}>Register</Link>
-    </section> );
+    return ( <Menubar className="flex justify-between ">
+       <KanbanButton variant={"outline"} size={"xs"} className=""><Link href={"/"}>Home</Link></KanbanButton>
+        <div className="flex gap-3.5">
+            {isAuthenticated ? <KanbanButton variant={"outline"} size={"xs"} className=" cursor-pointer" onClick={signOut}>Sign out</KanbanButton> : <KanbanButton variant={"outline"} size={"xs"}><Link href={"/signin"}>Sign In</Link></KanbanButton> }
+            <KanbanButton variant={"outline"} size={"xs"}><Link href={"/register"}>Register</Link>  </KanbanButton>
+        </div>
+    </Menubar> );
 }
  
 export default Navbar;
