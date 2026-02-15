3eb0fb3bce0fb63c2621806d57a80827d5247b2b
Joseph Martire <joemart06@gmail.com>
2026-02-15 13:46:44 -0400

    fix: Add rerouting to sign out

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,18 +1,23 @@
 "use client"
 import { useAuthenticated , useSignOut } from "@nhost/nextjs";
 import { Menubar } from "@/ui/menubar";
-
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import KanbanButton from "../KanbanButton";
 
 const Navbar = () => {
     const isAuthenticated = useAuthenticated()
     const {signOut} = useSignOut()
+    const router = useRouter()
+    const handleSignOut : React.MouseEventHandler<HTMLButtonElement> = () =>{
+        signOut()
+        router.push("/signin")
+    }
 
-    return ( <Menubar className="flex justify-between ">
-       <KanbanButton variant={"outline"} size={"xs"} className=""><Link href={"/"}>Home</Link></KanbanButton>
+    return ( <Menubar className="flex justify-between bg-prime border-prime">
+       <KanbanButton variant={"outline"} size={"xs"}><Link href={"/"}>Home</Link></KanbanButton>
         <div className="flex gap-3.5">
-            {isAuthenticated ? <KanbanButton variant={"outline"} size={"xs"} className=" cursor-pointer" onClick={signOut}>Sign out</KanbanButton> : <KanbanButton variant={"outline"} size={"xs"}><Link href={"/signin"}>Sign In</Link></KanbanButton> }
+            {isAuthenticated ? <KanbanButton variant={"outline"} size={"xs"} className=" cursor-pointer" onClick={handleSignOut}>Sign out</KanbanButton> : <KanbanButton variant={"outline"} size={"xs"}><Link href={"/signin"}>Sign In</Link></KanbanButton> }
             <KanbanButton variant={"outline"} size={"xs"}><Link href={"/register"}>Register</Link>  </KanbanButton>
         </div>
     </Menubar> );
